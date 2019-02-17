c2d79b143393fb6925ae2c1fb4b97a23da42bb0e
edvinteodoro <edvinteodoro2@gmail.com>
2019-02-16 23:43:07 -0600

    #1 Inicio del proyecto

---
+++ src/run.java
@@ -0,0 +1,12 @@
+
+import frames.framePrincipal;
+
+
+public class run {
+
+    public static void main(String[] args) {
+        framePrincipal frame= new framePrincipal();
+        frame.setVisible(true);
+    }
+    
+}
